f65db316dc3c4bc210cf5f74e50a11e516004934
valenadels <vadelsflugel@fi.uba.ar>
2025-04-16 18:47:33 -0300

    LTO

---
+++ cmake/LTO.cmake
@@ -0,0 +1,11 @@
+function(target_lto TARGET)
+    include(CheckIPOSupported)
+    check_ipo_supported(RESULT result OUTPUT output)
+
+    if(result)
+        message(STATUS "IPO/LTO is supported!")
+        set_property(TARGET ${TARGET} PROPERTY INTERPROCEDURAL_OPTIMIZATION)
+    else()
+        message(WARNING "IPO/LTO is not supported!")
+    endif()
+endfunction(target_lto)
