f0290f45a7aeb0756945d385fa9d1df7e283e807
valenadels <vadelsflugel@fi.uba.ar>
2025-08-17 20:48:35 -0300

    fix tests

--- src/resolution.cpp
+++ src/resolution.cpp
@@ -1,6 +1,7 @@
+#include <map>
+#include <stdexcept>
 #include "resolution.h"
 #include "errors.h"
-#include <stdexcept>
 
 namespace H3Origins {
     constexpr H3Index ORIGIN_RES_0 = 0x8047fffffffffff;
@@ -8,33 +9,33 @@ namespace H3Origins {
     constexpr H3Index ORIGIN_RES_2 = 0x824607fffffffff;
 }
 
-constexpr int MAX_HEX_BY_RES[] = {
-    122,
-    842,
-    5882,
-};
-
 namespace H3ResolutionLimits {
-    constexpr int MIN = 0;
     constexpr int MAX = 2;
 }
 
+const std::map<int, H3Index> ORIGIN_BY_RES = {
+    {0, H3Origins::ORIGIN_RES_0},
+    {1, H3Origins::ORIGIN_RES_1},
+    {2, H3Origins::ORIGIN_RES_2},
+};
+
+const std::map<int, int> MAX_HEX_BY_RES = {
+    {0, 122},
+    {1, 842},
+    {2, 5882},
+};
+
 H3Index getOriginForResolution(const int res) {
-    switch (res) {
-        case H3ResolutionLimits::MIN: return H3Origins::ORIGIN_RES_0;
-        case 1: return H3Origins::ORIGIN_RES_1;
-        case H3ResolutionLimits::MAX: return H3Origins::ORIGIN_RES_2;
-        default:
-            throw std::out_of_range{Errors::RESOLUTION_NOT_SUPPORTED};
-    }
+    const auto it = ORIGIN_BY_RES.find(res);
+    if (it == ORIGIN_BY_RES.end())
+        throw std::out_of_range{Errors::RESOLUTION_NOT_SUPPORTED};
+    return it->second;
 }
 
-//TODO hablar con hamelin
 int getResolution(const int satellites) {
-    for (int res = 0; res < H3ResolutionLimits::MAX; res++) {
-        if (satellites <= MAX_HEX_BY_RES[res])
+    for (auto const& [res, max] : MAX_HEX_BY_RES) {
+        if (satellites <= max)
             return res;
     }
-
     return H3ResolutionLimits::MAX;
 }
--- tests/resolution.cpp
+++ tests/resolution.cpp
@@ -2,31 +2,20 @@
 #include "resolution.h"
 
 TEST(ResolutionTest, GetOriginForValidResolutions) {
+    EXPECT_EQ(getOriginForResolution(0), 0x8047fffffffffff);
     EXPECT_EQ(getOriginForResolution(1), 0x81463ffffffffff);
     EXPECT_EQ(getOriginForResolution(2), 0x824607fffffffff);
-    EXPECT_EQ(getOriginForResolution(3), 0x834600fffffffff);
-    EXPECT_EQ(getOriginForResolution(4), 0x8446001ffffffff);
-    EXPECT_EQ(getOriginForResolution(5), 0x85460003fffffff);
 }
 
 TEST(ResolutionTest, GetOriginForInvalidResolutionThrows) {
-    EXPECT_THROW(getOriginForResolution(0), std::out_of_range);
     EXPECT_THROW(getOriginForResolution(6), std::out_of_range);
     EXPECT_THROW(getOriginForResolution(999), std::out_of_range);
 }
 
 TEST(ResolutionTest, GetResolutionValidSatellites) {
-    EXPECT_EQ(getResolution(50), 1);
-    EXPECT_EQ(getResolution(110), 1);
-    EXPECT_EQ(getResolution(200), 2);
-    EXPECT_EQ(getResolution(830), 2);
-    EXPECT_EQ(getResolution(1000), 3);
-    EXPECT_EQ(getResolution(5870), 3);
-    EXPECT_EQ(getResolution(41150), 4);
-    EXPECT_EQ(getResolution(288110), 5);
-}
-
-TEST(ResolutionTest, GetResolutionTooManySatellitesThrows) {
-    EXPECT_THROW(getResolution(2016831), std::out_of_range);
-    EXPECT_THROW(getResolution(99999999), std::out_of_range);
+    EXPECT_EQ(getResolution(50), 0);
+    EXPECT_EQ(getResolution(122), 0);
+    EXPECT_EQ(getResolution(200), 1);
+    EXPECT_EQ(getResolution(830), 1);
+    EXPECT_EQ(getResolution(5883), 2);
 }
