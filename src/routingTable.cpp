e7c645470cd8cf3d19777c2d962120f18fba1ccb
Gastón Frenkel <gst.frenkel@gmail.com>
2025-12-10 15:16:56 -0300

    add docs

--- src/routingTable.cpp
+++ src/routingTable.cpp
@@ -10,7 +10,7 @@ RoutingTable::RoutingTable(Antop* antop) {
     this->antop = antop;
 }
 
-// Returns true if the routing info to source should be updated.
+// Returns true if the routing information for a given destination should be upserted.
 bool shouldUpdateSrcInfo(const RoutingInfo &routingInfoToSrc, const int curDistance) {
     return routingInfoToSrc.nextHop == 0 || routingInfoToSrc.distance > curDistance;
 }
@@ -19,6 +19,7 @@ bool isLoop(const int storedDistance, const int curDistance) {
     return storedDistance != 0 && storedDistance + THRESHOLD_DISTANCE < curDistance;
 }
 
+// Flags the target index as visited in the bitmap.
 void flagTargetAsVisited(std::bitset<NEIGHBORS>& bitmap, const std::vector<H3Index>& candidates, const H3Index target) {
     auto curNeighbor = MSB_MASK;
 
@@ -33,7 +34,8 @@ void flagTargetAsVisited(std::bitset<NEIGHBORS>& bitmap, const std::vector<H3Ind
     throw std::runtime_error("Sender not found in candidates");
 }
 
-H3Index findNextUnvisitedNeighbor(std::bitset<NEIGHBORS>& bitmap, const std::vector<H3Index>& candidates, const H3Index fallback) {
+// Returns the best unvisited candidate. If none is found, findNextCandidate returns the provided fallback.
+H3Index findNextCandidate(std::bitset<NEIGHBORS>& bitmap, const std::vector<H3Index>& candidates, const H3Index fallback) {
     auto curNeighbor = MSB_MASK;
 
     for (const auto candidate : candidates) {
@@ -47,6 +49,7 @@ H3Index findNextUnvisitedNeighbor(std::bitset<NEIGHBORS>& bitmap, const std::vec
     return fallback;
 }
 
+// Retrieves a candidate for a packet's next hop.
 H3Index RoutingTable::findNextHop(const H3Index cur, const H3Index src, const H3Index dst, const H3Index sender, const int curDistance) {
     const PairTableKey pairTableKey{src, dst};
     const int storedDistance = pairTable[pairTableKey];
@@ -75,18 +78,19 @@ H3Index RoutingTable::findNextHop(const H3Index cur, const H3Index src, const H3
     return findNewNeighbor(cur, dst, sender);
 }
 
+// Retrieves a new candidate for a packet's next hop.
 H3Index RoutingTable::findNewNeighbor(const H3Index cur, const H3Index dst, const H3Index sender) {
     auto bitmap = routingTable[dst].visitedBitmap;
     const std::vector<H3Index> candidates = antop->getHopCandidates(cur, dst, 0);
 
     flagTargetAsVisited(bitmap, candidates, sender);
     
-    const H3Index nextNeighbor = findNextUnvisitedNeighbor(bitmap, candidates, sender);
+    const H3Index nextCandidate = findNextCandidate(bitmap, candidates, sender);
 
-    routingTable[dst].nextHop = nextNeighbor;
+    routingTable[dst].nextHop = nextCandidate;
     routingTable[dst].visitedBitmap = bitmap;
 
-    return nextNeighbor;
+    return nextCandidate;
 }
 
 int RoutingTable::getAntopResolution() const {
