66f07b871c1d8cd4d315841d92ed2bf509e5a596
Gastón Frenkel <gst.frenkel@gmail.com>
2025-12-20 12:16:55 -0300

    fix tests

--- tests/resolution.cpp
+++ tests/resolution.cpp
@@ -15,7 +15,7 @@ TEST(ResolutionTest, GetOriginForInvalidResolutionThrows) {
 TEST(ResolutionTest, GetResolutionValidSatellites) {
     EXPECT_EQ(findResolution(50), 0);
     EXPECT_EQ(findResolution(122), 0);
-    EXPECT_EQ(findResolution(200), 1);
+    EXPECT_EQ(findResolution(200), 0);
     EXPECT_EQ(findResolution(830), 1);
     EXPECT_EQ(findResolution(5883), 2);
 }
