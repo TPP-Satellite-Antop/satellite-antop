e9948e302951a4b75ddf30ec4bfe6f4fc697b5e7
Gastón Frenkel <gst.frenkel@gmail.com>
2026-01-02 20:53:08 -0300

    remove placeholder test

--- tests/antop.cpp
+++ tests/antop.cpp
@@ -7,31 +7,3 @@ TEST(AntopTest, NeighborRouting) {
 
     ASSERT_EQ(antop.getHopCandidates(0x8081fffffffffff, 0x8057fffffffffff)[0], 0x8057fffffffffff);
 }
-
-/*
-TEST(AntopTest, Aaa) {
-    Antop antop{};
-    antop.init(0);
-
-    constexpr H3Index src = 0x8067fffffffffff;
-    constexpr H3Index dst = 0x8021fffffffffff;
-
-    std::cout << std::hex << 0x8067fffffffffff << ": " << antop.distance(src, dst) << std::endl;
-    for (const auto candidate : antop.getHopCandidates(src, dst)) {
-        std::cout << std::hex << candidate << ": " << std::dec << antop.distance(candidate, dst) << std::endl;
-    }
-
-    std::cout << std::endl;
-
-    for (const auto candidate : antop.getHopCandidates(0x808ffffffffffff, dst)) {
-        std::cout << std::hex << candidate << ": " << std::dec << antop.distance(candidate, dst) << std::endl;
-    }
-
-    std::cout << std::endl;
-
-    for (const auto candidate : antop.getHopCandidates(0x8093fffffffffff, dst)) {
-        std::cout << std::hex << candidate << ": " << std::dec << antop.distance(candidate, dst) << std::endl;
-    }
-
-    std::cout << std::endl;
-}*/
